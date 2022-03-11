36154dedbbe2378cd4177c12addd9863e81d1b66
Jeremie MALUEKI NGOMBI <jeremie.malueki@hotmail.fr>
2022-03-11 09:58:22 +0100

    1er commit

---
+++ main.ml
@@ -0,0 +1,11 @@
+let _ = 
+    try
+        let lexbuf = Lexing.from_channel stdin in (*lexeur lancé sur stdin *)
+        while true do (*on ne s'arrete pas *)
+            Parseur.main Lexeur.token lexbuf (*parseur ligne*)
+        done
+    with
+        | Lexeur.Eof -> exit 0 (*impossible*)
+        | Lexeur.TokenInconu (*erreur de lexing*)
+        | Parsing.Parse_error -> (*erreur de parsing*)
+            Printf.printf ("Ceci n'est pas une expression arithmetique\n\r")
