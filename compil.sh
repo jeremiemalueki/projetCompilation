58b1795b0d183d1a49d029f9b3c22aaf85b6bb61
Jeremie MALUEKI NGOMBI <jeremie.malueki@hotmail.fr>
2022-04-25 15:57:12 +0200

    merge intro_ast p0.0

--- compil.sh
+++ compil.sh
@@ -2,5 +2,7 @@
 
 ocamllex lexeur.mll
 ocamlyacc parseur.mly
-ocamlc -c parseur.mli AST.ml AST.mli lexeur.ml parseur.ml main.ml
-ocamlc -o main lexeur.cmo parseur.cmo AST.cmo main.cmo
+ocamlc -c AST.ml
+ocamlc -i AST.ml > AST.mli
+ocamlc -c parseur.mli lexeur.ml parseur.ml main.ml
+ocamlc -o main lexeur.cmo parseur.cmo AST.cmo main.cmo 
